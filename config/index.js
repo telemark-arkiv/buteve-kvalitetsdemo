9662e8c0bc7d93cb1f150feb7d8a49e9f52525b0
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-05-30 11:51:37 +0200

    Initial commit

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var config = {
   SERVER_PORT: 3000,
   FIREBASE_URL: 'your-firebase-url',
   FIREBASE_DB: 'your-firebase-db',
-  ACHECKER_ID: 'your-achecker-key',
-  GOOGLE_API_KEY: 'your-google-key'
+  ACHECKER_ID: 'your-achecker-id',
+  GOOGLE_API_KEY: 'your-google-api-key'
 };
 
 module.exports = config;
